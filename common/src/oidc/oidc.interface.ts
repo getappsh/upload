5817cd0c2dd845faf01b9c8e2f413c6c8ecf5266
David Klein <davidk@ravtech.co.il>
2025-01-05 17:38:54 +0200

    Refactor user retrieval to use POST method with search parameters

--- common/src/oidc/oidc.interface.ts
+++ common/src/oidc/oidc.interface.ts
@@ -1,4 +1,6 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { UserDto } from "../dto/oidc/dto/user.dto";
+import { IsBoolean, IsOptional, IsString } from "class-validator";
 
 export interface OidcService {
   authenticate(): Promise<void>
@@ -6,10 +8,29 @@ export interface OidcService {
   inviteUser(params?: UserDto): Promise<void>
 }
 
-export interface UserSearchDto {
+export class UserSearchDto {
+  
+  @ApiProperty({required:false})
+  @IsOptional()
+  @IsString()
   email?: string;
+  
+  @ApiProperty({required:false})
+  @IsOptional()
+  @IsString()
   firstName?: string;
+  
+  @ApiProperty({required:false})
+  @IsOptional()
+  @IsString()
   lastName?: string;
+  
+  @ApiProperty({required:false})
+  @IsOptional()
+  @IsString()
   username?: string;
+  
+  @IsOptional()
+  @IsBoolean()
   exact?: boolean;
 }
