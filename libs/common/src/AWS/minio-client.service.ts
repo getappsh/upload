0acc3696688b4c4203963aea9ebf221cd00d50a4
Ari Havlin <ayhavlin@gmail.com>
2025-01-19 13:10:34 +0200

    Update minio client

--- libs/common/src/AWS/minio-client.service.ts
+++ libs/common/src/AWS/minio-client.service.ts
@@ -1,11 +1,11 @@
-import { Injectable, OnApplicationBootstrap, Logger } from "@nestjs/common";
+import { Injectable, OnApplicationBootstrap, Logger, OnModuleInit } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import * as Minio from 'minio';
 import stream from 'stream';
 
 
 @Injectable()
-export class MinioClientService implements OnApplicationBootstrap{
+export class MinioClientService implements OnModuleInit{
 
   private readonly logger = new Logger(MinioClientService.name);
   private readonly client: Minio.Client;
@@ -18,9 +18,17 @@ export class MinioClientService implements OnApplicationBootstrap{
     const accessKey = this.configService.get('ACCESS_KEY_ID');
     const secretKey = this.configService.get('SECRET_ACCESS_KEY');
     const useSSL = this.configService.get('MINIO_USE_SSL') === 'true';
-   
+
+    let port: number | undefined;
+    const portMatch = endpoint.match(/:(\d+)$/);
+    if (portMatch) {
+      port = parseInt(portMatch[1], 10);
+      endpoint = endpoint.replace(/:\d+$/, '');
+    }
+
     this.client = new Minio.Client({
       endPoint: endpoint,
+      port: port,
       accessKey: accessKey,
       secretKey: secretKey,
       useSSL: useSSL
@@ -36,14 +44,17 @@ export class MinioClientService implements OnApplicationBootstrap{
     return this.client.presignedPutObject(bucketName, objectKey, expression || this.uploadUrlExpire);
   }
 
+  deleteObjects(bucketName: string, objectsKey: string[] | string): Promise<void> {
+    const objects = Array.isArray(objectsKey) ? objectsKey : [objectsKey];
+    return this.client.removeObjects(bucketName, objects);
+  }
 
   generatePresignedDownloadUrl(bucketName: string, objectKey: string, expression?: number): Promise<string> {
     return this.client.presignedGetObject(bucketName, objectKey, expression || this.downloadUrlExpire);
   }
 
   getObject(bucketName: string, objectKey: string): Promise<stream.Readable> {
-    return this.client.getObject(bucketName, objectKey);
-    
+    return this.client.getObject(bucketName, objectKey);    
   }
   
   async getObjectStat(bucketName: string, objectKey: string): Promise<Minio.BucketItemStat | undefined> {
@@ -62,9 +73,13 @@ export class MinioClientService implements OnApplicationBootstrap{
     return this.client.bucketExists(bucketName)
   }
 
-  async onApplicationBootstrap() {
+  async onModuleInit() {
     const bucketName = this.configService.get('BUCKET_NAME');
-    const exists = await this.bucketExists(bucketName);
-    this.logger.debug(`Bucket "${bucketName}" exists: ${exists}`)
+    try{
+      const exists = await this.bucketExists(bucketName);
+      this.logger.debug(`Bucket "${bucketName}" exists: ${exists}`)
+    }catch(err){
+      this.logger.error(`Error checking bucket "${bucketName}": ${err}`)
+    }
   }
 }
