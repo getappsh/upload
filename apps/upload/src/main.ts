659fb769f7248f1bffe0706aabd4f79cae76c6b8
mataniss <matanissler@gmail.com>
2025-12-17 15:42:07 +0200

    revert change in main file

--- apps/upload/src/main.ts
+++ apps/upload/src/main.ts
@@ -3,12 +3,11 @@ dotenv.config();
 import apm from 'nestjs-elastic-apm';
 
 import { NestFactory } from '@nestjs/core';
-import { MicroserviceOptions, RpcException } from '@nestjs/microservices';
+import { MicroserviceOptions } from '@nestjs/microservices';
 import { CustomRpcExceptionFilter } from './rpc-exception.filter';
 import { UploadModule } from './upload.module';
 import { MSType, MicroserviceName, MicroserviceType, getClientConfig } from '@app/common/microservice-client';
 import { GET_APP_LOGGER } from '@app/common/logger/logger.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
@@ -23,19 +22,6 @@ async function bootstrap() {
     }
   );
   app.useLogger(app.get(GET_APP_LOGGER))
-  app.useGlobalPipes(new ValidationPipe({
-    transform: true,
-    whitelist: true,
-    exceptionFactory: (errors) => {
-      const messages = errors.map(error => 
-        Object.values(error.constraints || {}).join(', ')
-      ).join('; ');
-      return new RpcException({
-        statusCode: 400,
-        message: messages
-      });
-    }
-  }));
   app.useGlobalFilters(new CustomRpcExceptionFilter())
   app.listen()
 }
