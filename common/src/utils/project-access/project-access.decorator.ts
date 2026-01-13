e72814cc46c3a1a7d2dd10e3ce2f7cebe222e9ee
mataniss <matanissler@gmail.com>
2026-01-13 12:17:32 +0200

    fix code 403 issues

--- common/src/utils/project-access/project-access.decorator.ts
+++ common/src/utils/project-access/project-access.decorator.ts
@@ -22,3 +22,17 @@ export const ValidateProjectAnyAccess = (...roles: string[]) => {
     UseGuards(ProjectAccessGuard)
   )
 }
+
+/**
+ * Validates that the user has access to all projects referenced in the DTO
+ * @param projectExtractor - Function that extracts project names from the request DTO
+ * @param roles - Optional roles to check
+ */
+export const ValidateProjectListAccess = (projectExtractor: string | ((dto: any) => string[]), ...roles: string[]) => {
+  return applyDecorators(
+    SetMetadata('roles', roles),
+    SetMetadata('validateProjectListAccess', true),
+    SetMetadata('projectExtractor', projectExtractor),
+    UseGuards(ProjectAccessGuard)
+  )
+}
