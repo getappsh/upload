d1f42a71d243e0302b7edefa32e579cc0af5b89d
mataniss <matanissler@gmail.com>
2026-02-08 12:20:53 +0200

    move allow any device name to a constant

---
+++ libs/common/src/rules/constants.ts
@@ -0,0 +1,14 @@
+/**
+ * Default rule constants
+ */
+
+/**
+ * Name of the default policy that allows all devices to download a component.
+ * This rule is automatically applied to all existing and new releases unless manually removed.
+ */
+export const DEFAULT_ALLOW_ALL_DEVICES_RULE_NAME = 'Allow All Devices';
+
+/**
+ * UUID for the default "Allow All Devices" policy rule
+ */
+export const DEFAULT_ALLOW_ALL_DEVICES_RULE_ID = '00000000-0000-0000-0000-000000000001';
