0f05024ff65262b75190569861ec3b4cc4a6e115
mataniss <matanissler@gmail.com>
2026-04-20 10:53:52 +0300

    wip

--- common/src/database/entities/enums.entity.ts
+++ common/src/database/entities/enums.entity.ts
@@ -173,7 +173,9 @@ export enum ProjectType {
     APPLICATION = "application",
     LIB = "lib",
     BUNDLE = "bundle",
-    INFRA = "infra"
+    INFRA = "infra",
+    CONFIG = "config",
+    CONFIG_MAP = "config_map"
   }
 
 
